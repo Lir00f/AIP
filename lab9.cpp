cb480f4897bf127dd7dce40e563bfe6cd95a7439
Elvin <93262078+Lir00f@users.noreply.github.com>
2021-11-27 19:21:10 +0300

    Update lab9.cpp

--- lab9.cpp
+++ lab9.cpp
@@ -2,31 +2,29 @@
 using namespace std;
 
 void main() {
-	// Лабораторная работа 9
-
-	cout << "Task 1" << endl;
+	setlocale (LC_ALL, "RUSSIAN");
+	cout << "Первое Задание" << endl;
 	char ch = '0';
-	cout << "Enter char" << endl;
+	cout << "Введите символ" << endl;
 	do {
 		cin >> ch;
 		if (ch == '.')
 			break;
 		cout << int(ch) << endl;
 	} while (ch != '.');
-	cout << "Finish!" << endl << endl;
 
-	cout << "Task 2" << endl;
+	cout << "Задание 2" << endl;
 	int sum = 0, num;
-	cout << "Enter the sequence of numbers" << endl;
+	cout << "Введите последовательность чисел" << endl;
 	while (sum <= 100)
 	{
 		cin >> num;
 		sum += num * num;
 	}
-	cout << "Sum = " << sum << endl << endl; 
+	cout << sum << endl; 
 
 	cout << "Task 3" << endl;
-	int mult = 1, min = 2147483647;
+	int bebr = 1, min = 2147483647;
 	sum = 0, num = 1;
 	while (num != 0) {
 		cin >> num;
@@ -37,9 +35,9 @@ void main() {
 		if (num < min && num != 0)
 			min = num;
 	}
-	cout << "Sum = " << sum << endl << "Mult = " << mult << endl << "Min = " << min << endl << endl;
+	cout << "Сумма равна :" << sum << endl << "bebr =  " << bebr << endl << "min = " << min << endl << endl;
 
-	cout << "Task 4.1" << endl;
+	cout << "Задание 4, часть первая" << endl;
 	int i, j;
 	for (i = 1; i <= 5; i++) {
 		for (j = 5; j >= i; j--) {
@@ -47,7 +45,7 @@ void main() {
 		}
 		cout << endl;
 	}
-	cout << endl << "Task 4.2" << endl;
+	cout << endl << "Задание 4, часть вторая" << endl;
 	for (i = 1; i <= 5; i++) {
 		for (j = 1; j <= 6 - i; j++) {
 			cout << j << ' ';
@@ -55,24 +53,24 @@ void main() {
 		cout << endl;
 	}
 
-	cout << endl << "Task 5" << endl;
+	cout << endl << "Задание 5" << endl;
 	int type, seconds = 0, minutes = 0, hours = 0;
-	cout << "Enter 1 for minutes and 2 for hours" << endl;
+	cout << "Введите либо min для подсчета минут, либо hour для подсчета часов: " << endl;
 	cin >> type;
 	switch (type) {
-	case 1:
+	case min:
 		cout << "Enter minutes: ";
 		cin >> minutes;
 		hours = minutes / 60;
 		seconds = minutes * 60;
 		break;
-	case 2:
+	case hour:
 		cout << "Enter hours: ";
 		cin >> hours;
 		minutes = hours * 60;
 		seconds = minutes * 60;
 		break;
 	}
-	cout << "Seconds: " << seconds << ". Minutes: " << minutes << ". Hours: " << hours << endl << endl;
+	cout << "Секунд: " << seconds <<endl<< " Минут:  " <<endl<< minutes << "Часов: " << hours << endl << endl;
 	system("pause");
 }
