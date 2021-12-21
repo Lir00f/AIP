df6decf11f3d9f0e3d713f655c1ae3dc9b083d39
Elvin <93262078+Lir00f@users.noreply.github.com>
2021-12-21 10:30:14 +0300

    Create lab8.cpp

---
+++ lab8.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	// Часть 1
+	cout << "Первая часть лабораторной работы #8" << endl << endl;
+
+
+	int i, j, s = 0;
+	for (i = 2; ; i += 2)
+	{
+		cout << "Введите значение переменной j : ";
+		cin >> j;
+		if (j != 0)
+			s = s + i * j;
+		else
+			break;
+	}
+	cout << "Значение s = " << s << endl;
+
+
+	//Часть 2
+	system("pause");
+
+
+	cout << "Вторая часть Лабораторной работы #8" << endl << endl;
+	int n, u;
+	double t, a, f = 0;
+	cout << "Введите значение n: ";
+	cin >> n;
+	cout << "Введите  значение t: ";
+	cin >> t;
+	for (u = 0; u <= n; u++)
+	{
+		cout << "Введите значение a: ";
+		cin >> a;
+		for (
+			int i = 1;
+			i < n - u;
+			i++
+			)
+		{
+			t = t * t;
+		};
+		f = f + a *t;
+	}
+	cout << "Значение f = " << f << endl << "Нажмите любую кнопку дважды для выхода из программы";
+	cin.get();
+	cin.get();
+}
